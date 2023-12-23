14c4de7f57ecfd421f235582e1da7cb437e723fb
rkashif3 <rkashif3@uwo.ca>
2023-12-23 01:15:10 -0500

    Changed cargo name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/", web::post().to(echo))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
---
+++ src/room.rs
@@ -0,0 +1,11 @@
+pub struct Room {
+    id: u32,
+    name: String,
+}
+
+impl Room {
+
+    pub fn new() -> Room {
+        // TODO: get a random id
+    }
+}
